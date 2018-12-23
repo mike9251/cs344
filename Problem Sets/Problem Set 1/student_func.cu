107e6d1fbba74a7fe2be3d85384ed2425447a12b
Mikhail <soapbox92@gmail.com>
2018-12-23 12:04:05 +0300

    Solution

--- Problem Sets/Problem Set 1/student_func.cu
+++ Problem Sets/Problem Set 1/student_func.cu
@@ -50,6 +50,13 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+  uint thread_id_x = blockIdx.x * blockDim.x + threadIdx.x;
+  uint thread_id_y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  uint offset = numCols * thread_id_y + thread_id_x;
+
+  if ( (thread_id_x < numCols) && (thread_id_y < numRows) )
+    greyImage[offset] = .299f * rgbaImage[offset].x + .587f * rgbaImage[offset].y + .114f * rgbaImage[offset].z;
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -57,8 +64,9 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+  const dim3 blockSize(16, 16, 1);  //TODO
+  std::cout << numCols / blockSize.x << " | (ceil) " <<ceil(numCols / blockSize.x)<<std::endl;
+  const dim3 gridSize( ceil(numCols / blockSize.x) + 1, ceil(numRows / blockSize.y) + 1, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
